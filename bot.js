565d7889711fa65e6f724257d11f859ed6fdd787
ShawnToubeau <shawntoubeau@gmail.com>
2018-09-12 14:20:54 -0400

    Clean up

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 const order = 4; // length of each n-gram
 let nGrams = {};
 
-// const Bot = new Twit({
-//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//     access_token: process.env.TWITTER_ACCESS_TOKEN,
-//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET  
-// });
+const Bot = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET  
+});
 
 function pickRandomStart(lyrics) {
     const random = Math.floor(Math.random()*lyrics.length)
@@ -37,13 +37,13 @@ function tweet() {
              let currentGram = pickRandomStart(lyrics);
              
             // checks to see if the start of the tweet doesn't start 
-            // with punctuation or special characters and ends with a space
+            // with punctuation or special characters
             while (!currentGram.match(/^[0-9a-zA-Z]+$/)) { 
                 currentGram = pickRandomStart(lyrics);
             }
             let tweet = currentGram;
 
-            // runs until char limit is reached while finishing the last word it was on
+            // runs until char limit is reached and tries finishing the last word it was on
             for (let j = 0; (j < 150) || (tweet.charAt(j).match(/^[0-9a-zA-Z]+$/)); j++) {
                 const possibilities = nGrams[currentGram];
                 const next = possibilities[Math.floor(Math.random()*possibilities.length)];
@@ -53,11 +53,11 @@ function tweet() {
             }
             console.log(tweet)
             
-            // Bot.post('statuses/update', {status: tweet}, function(error, tweet, response) {
-            //     if (error) {
-            //         console.log("Error making post. ", error.message);
-            //     };
-            // });
+            Bot.post('statuses/update', {status: tweet}, function(error, tweet, response) {
+                if (error) {
+                    console.log("Error making post. ", error.message);
+                };
+            });
         }
     });
 }
